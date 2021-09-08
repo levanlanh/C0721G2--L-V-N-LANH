9a9fcbab4a0c62c4f11789c4fc309c0f76ca2869
C0721G2--L-V-N-Lanh <levanlanh1802@gmail.com>
2021-09-08 21:42:32 +0700

    module2

--- module_2/src/bai5_access_modifier_static_menthod_static_property/practice/TestCar.java
+++ module_2/src/bai5_access_modifier_static_menthod_static_property/practice/TestCar.java
@@ -2,10 +2,15 @@ package bai5_access_modifier_static_menthod_static_property.practice;
 
 public class TestCar {
     public static void main(String[] args) {
-        Car car1 = new Car("toyota","abc");
+
+        Car car1 = new Car("Mazda 3", "Skyactiv 3");
+
         System.out.println(Car.numberOfCars);
-        Car ca2 = new Car("Suzuki","xyz");
+
+        Car car2 = new Car("Mazda 6", "Skyactiv 6");
+
         System.out.println(Car.numberOfCars);
+
     }
     }
 
