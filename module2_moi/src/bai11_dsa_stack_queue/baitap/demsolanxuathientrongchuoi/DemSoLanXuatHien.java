3898afadd69f26ec6ff3f4b500e77f8eeba4b0e9
C0721G2--L-V-N-Lanh <levanlanh1802@gmail.com>
2021-10-22 08:30:15 +0700

    module2

--- module2_moi/src/bai11_dsa_stack_queue/baitap/demsolanxuathientrongchuoi/DemSoLanXuatHien.java
+++ module2_moi/src/bai11_dsa_stack_queue/baitap/demsolanxuathientrongchuoi/DemSoLanXuatHien.java
@@ -2,31 +2,30 @@ package bai11_dsa_stack_queue.baitap.demsolanxuathientrongchuoi;
 
 
 import java.util.Arrays;
-import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class DemSoLanXuatHien {
     public static void main(String[] args) {
         String str = "Hôm nay trời đẹp đẹp quá trời luôn ";
-
         // chuyển thành chữ thường.
         str.toLowerCase();
-
-        // tách chuỗi thành từ.
+        // tách chuỗi thành từ đưa vào mảng.
         String[] arr = str.split(" ");
         System.out.println(Arrays.toString(arr));
         // dùng treeMap có 2 cặp key và value.
-        TreeMap<String, Integer> treeMap = new TreeMap<>();
+        Map<String, Integer> treeMap = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             String key = arr[i];
+            // contains kiểm tra xem có tồn tại hay ko.
             if (treeMap.containsKey(key)) {
-                treeMap.put(key, treeMap.get(key) + 1);
+                int value = treeMap.get(key); // lấy value
+                treeMap.put(key, value + 1);
             } else {
                 treeMap.put(key, 1);
             }
         }
         System.out.println(treeMap);
-
-
     }
 }
