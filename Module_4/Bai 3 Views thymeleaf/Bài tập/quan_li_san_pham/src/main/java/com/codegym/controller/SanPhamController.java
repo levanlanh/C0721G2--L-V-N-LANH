65b621f63044987a302d80d7f03d280ae24f0e5c
C0721G2--L-V-N-Lanh <levanlanh1802@gmail.com>
2021-12-20 18:35:27 +0700

    module4

--- Module_4/Bai 3 Views thymeleaf/Bài tập/quan_li_san_pham/src/main/java/com/codegym/controller/SanPhamController.java
+++ Module_4/Bai 3 Views thymeleaf/Bài tập/quan_li_san_pham/src/main/java/com/codegym/controller/SanPhamController.java
@@ -1,4 +1,62 @@
 package com.codegym.controller;
 
+import com.codegym.model.SanPham;
+import com.codegym.service.ISanPhamService;
+import com.codegym.service.impl.SanPhamService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/sanPham")
 public class SanPhamController {
+    private final ISanPhamService sanPhamService = new SanPhamService();
+
+    @GetMapping("")
+    public String index(Model model){
+        List<SanPham> sanPhamList = sanPhamService.findAll();
+        model.addAttribute("sanPham",sanPhamList);
+        return "/index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model){
+        model.addAttribute("sanPham",new SanPham());
+        return "/create";
+    }
+    @PostMapping("/save")
+    public String save(@ModelAttribute("sanPham") SanPham sanPham ){
+        sanPham.setId((int)(Math.random()*1000));
+        sanPhamService.save(sanPham);
+        return "redirect:/sanPham";
+    }
+   @GetMapping("/edit")
+    public String edit(@RequestParam int id,Model model){
+        model.addAttribute("sanPham",sanPhamService.findById(id));
+        return "/edit";
+   }
+   @PostMapping("/update")
+    public String update(@ModelAttribute("sanPham") SanPham sanPham){
+        sanPhamService.update(sanPham.getId(),sanPham);
+        return "redirect:/sanPham";
+   }
+   @GetMapping("/delete")
+    public String delete(@RequestParam int id , Model model  ){
+        model.addAttribute("sanPham",sanPhamService.findById(id));
+        return "/delete";
+   }
+   @PostMapping("/delete")
+    public String delete(SanPham sanPham , RedirectAttributes attributes){
+        sanPhamService.remove(sanPham.getId());
+        attributes.addFlashAttribute("success","xóa thành công ");
+        return "redirect:/sanPham";
+   }
+    @GetMapping("/{id}/view")
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("sanPham", sanPhamService.findById(id));
+        return "/view";
+    }
 }
